b6f9438faa379e9efe18df94edbcfa21f654205f
Ricardo <ricardosanzh@gmail.com>
2018-10-25 12:21:55 +0200

    Add files via upload

---
+++ Ejemplos1_RSH_ScrollView/scrollViewExample/scrollViewExample/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  scrollViewExample
+//
+//  Created by Sanz Hinojosas, R. A. on 16/10/2018.
+//  Copyright © 2018 -. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var innerParentView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var orangeView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
